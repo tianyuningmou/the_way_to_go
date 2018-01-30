80b96885217642542afb5781d95cabecdbc894f6
tianyuningmou <tianyuningmou2009@126.com>
2018-01-30 14:36:09 +0800

    pointer

---
+++ 15_pointer.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var i1 = 5
+	fmt.Printf("An Integer: %d, it`s location in memory is %p\n", i1, &i1)
+	var intP *int
+	intP = &i1
+	fmt.Printf("The value at memory location %p is %d\n", intP, *intP)
+}
