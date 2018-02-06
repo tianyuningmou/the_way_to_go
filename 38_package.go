4b1e8eadfc41a05e5644746c32c8fd807f4ff6ba
tianyuningmou <tianyuningmou2009@126.com>
2018-02-06 11:40:03 +0800

    package

---
+++ 38_package.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"./pack"
+)
+
+func main() {
+	var test1 string
+	test1 = pack.ReturnStr()
+	fmt.Printf("ReturnStr from package1: %s\n", test1)
+	fmt.Printf("Integer from package1: %d\n", pack.Pack1Int)
+}
