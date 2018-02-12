19438016bb2ece12343697190e8bb62f5918d862
tianyuningmou <tianyuningmou2009@126.com>
2018-02-12 10:44:19 +0800

    varnumpar

--- 21_varnumpar.go
+++ 21_varnumpar.go
@@ -5,13 +5,13 @@ import "fmt"
 func main() {
 	x := min(1, 3, 2, 0)
 	fmt.Printf("The minimum is: %d\n", x)
-	arr := []int{7,9,3,5,1}
+	arr := []int{7, 9, 3, 5, 1}
 	x = min(arr...)
 	fmt.Printf("The minimum in the array arr is: %d", x)
 }
 
 func min(a ...int) int {
-	if len(a)==0 {
+	if len(a) == 0 {
 		return 0
 	}
 	min := a[0]
