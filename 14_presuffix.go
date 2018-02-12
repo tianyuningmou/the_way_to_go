ecd2491d75ff324707f193bff464af51dcaaa4ae
tianyuningmou <tianyuningmou2009@126.com>
2018-02-12 10:42:57 +0800

    presuffix

--- 14_presuffix.go
+++ 14_presuffix.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func main()  {
+func main() {
 	var str string = "This is an example of string"
 	fmt.Printf("T/F? Does the string \"%s\" have prefix %s? \n", str, "Th")
 	fmt.Printf("%t\n", strings.HasPrefix(str, "Th"))
