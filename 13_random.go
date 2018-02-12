d882a830947df8d18a4a544ae422a6f267b6e591
tianyuningmou <tianyuningmou2009@126.com>
2018-02-12 10:42:29 +0800

    random

--- 13_random.go
+++ 13_random.go
@@ -6,13 +6,13 @@ import (
 	"time"
 )
 
-func main()  {
-	for i := 0; i < 10; i ++ {
+func main() {
+	for i := 0; i < 10; i++ {
 		a := rand.Int()
 		fmt.Printf("%d /", a)
 	}
 
-	for i :=0; i < 5; i ++ {
+	for i := 0; i < 5; i++ {
 		r := rand.Intn(8)
 		fmt.Printf("%d /", r)
 	}
@@ -21,7 +21,7 @@ func main()  {
 	timens := int64(time.Now().Nanosecond())
 	rand.Seed(timens)
 
-	for i := 0; i < 10; i ++ {
+	for i := 0; i < 10; i++ {
 		fmt.Printf("%2.2f /", 100*rand.Float32())
 	}
 }
