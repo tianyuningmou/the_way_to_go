142a070590d04fcd65ce1bd12a646cbc61766f83
tianyuningmou <tianyuningmou2009@126.com>
2018-02-12 10:47:49 +0800

    switch_input

--- 54_switch_input.go
+++ 54_switch_input.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 )
 
 func main() {
@@ -19,23 +19,33 @@ func main() {
 	fmt.Printf("Your name is %s", input)
 	// For Unix: test with delimiter "\n", for Windows: test with "\r\n"
 	switch input {
-	case "Philip\r\n":  fmt.Println("Welcome Philip!")
-	case "Chris\r\n":   fmt.Println("Welcome Chris!")
-	case "Ivo\r\n":     fmt.Println("Welcome Ivo!")
-	default: fmt.Printf("You are not welcome here! Goodbye!")
+	case "Philip\r\n":
+		fmt.Println("Welcome Philip!")
+	case "Chris\r\n":
+		fmt.Println("Welcome Chris!")
+	case "Ivo\r\n":
+		fmt.Println("Welcome Ivo!")
+	default:
+		fmt.Printf("You are not welcome here! Goodbye!")
 	}
 
 	// version 2:
 	switch input {
-	case "Philip\r\n":  fallthrough
-	case "Ivo\r\n":     fallthrough
-	case "Chris\r\n":   fmt.Printf("Welcome %s\n", input)
-	default: fmt.Printf("You are not welcome here! Goodbye!\n")
+	case "Philip\r\n":
+		fallthrough
+	case "Ivo\r\n":
+		fallthrough
+	case "Chris\r\n":
+		fmt.Printf("Welcome %s\n", input)
+	default:
+		fmt.Printf("You are not welcome here! Goodbye!\n")
 	}
 
 	// version 3:
 	switch input {
-	case "Philip\r\n", "Ivo\r\n":   fmt.Printf("Welcome %s\n", input)
-	default: fmt.Printf("You are not welcome here! Goodbye!\n")
+	case "Philip\r\n", "Ivo\r\n":
+		fmt.Printf("Welcome %s\n", input)
+	default:
+		fmt.Printf("You are not welcome here! Goodbye!\n")
 	}
 }
